83629070c921a2bcd228dd7a215a49313b858b3c
Bree-codes <mwongera.mukami22@students.dkut.ac.ke>
2024-02-11 18:56:46 +0300

    working on it

--- QuickSortAlgorithm/src/Main.java
+++ QuickSortAlgorithm/src/Main.java
@@ -1,7 +1,12 @@
+import java.util.Arrays;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        int[] array = new int[3];
 
+        System.out.println(Arrays.toString(array));
     }
+
 }
--- QuickSortAlgorithm/src/QuickSort.java
+++ QuickSortAlgorithm/src/QuickSort.java
@@ -1,8 +1,20 @@
+import jdk.dynalink.linker.LinkerServices;
+
+import java.util.List;
+
 public class QuickSort {
 
     /*This method accepts a one dimensional array and sorts the data in ascending order*/
 
     public int[] quickSort(int[] array)
+    {
+
+
+        return null;
+    }
+
+
+    public List<int[]> partition(int[] array, int start, int end)
     {
 
 
