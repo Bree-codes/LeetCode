3e82d6cd19022f45a58c7450e62b49077060a100
Bree-codes <mwongera.mukami22@students.dkut.ac.ke>
2024-02-11 21:35:52 +0300

    working on it

--- QuickSortAlgorithm/src/Main.java
+++ QuickSortAlgorithm/src/Main.java
@@ -6,6 +6,18 @@ public class Main {
     public static void main(String[] args) {
         int[] array = new int[3];
 
+        QuickSort quickSort = new QuickSort();
+
+        try {
+
+
+            int[] result = quickSort.quickSort(array);
+        }
+        catch (ArrayIndexOutOfBoundsException e)
+        {
+            System.out.println("");
+        }
+
         System.out.println(Arrays.toString(array));
     }
 
--- QuickSortAlgorithm/src/QuickSort.java
+++ QuickSortAlgorithm/src/QuickSort.java
@@ -1,5 +1,6 @@
 import jdk.dynalink.linker.LinkerServices;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class QuickSort {
@@ -8,27 +9,51 @@ public class QuickSort {
 
     public int[] quickSort(int[] array)
     {
+        int start = 0;
+        int end = array.length-1;
+        List<int[]> list = partition(array, start, end);
+        if(start < end)
+        {
+            partition(array, start, list.getFirst()[0]-1);
+            partition(array, list.getFirst()[0]+1, end);
+        }
 
-
-        return null;
+        return array;
     }
 
 
     public List<int[]> partition(int[] array, int start, int end)
     {
+        int temp;
+
         //pivot for centering the array
         int pivot = start;
 
         while(start < end) {
-            while (start < array.length && end >= 0) {
+            while (start < array.length && end > 0) {
                 while (array[start] < pivot)
                     start++;
 
-                while (array[end] > pivot)
+                while (array[end] >= pivot)
                     end--;
+
+                //perform the swapping.
+                temp = array[start];
+                array[start] = array[end];
+                array[end] = temp;
             }
-            //perform the swapping.
+
         }
-        return null;
+        //place the pivot at it right full position.
+        temp = array[pivot];
+        array[pivot] = array[start];
+        array[start] = temp;
+
+        List<int[]> list = new ArrayList<>();
+
+        list.add(new int[]{start});
+        list.add(array);
+
+        return list;
     }
 }
