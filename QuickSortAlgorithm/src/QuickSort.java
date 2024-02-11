198df0be5eb64bfd66e9180812aa6cd10ff6c029
Bree-codes <mwongera.mukami22@students.dkut.ac.ke>
2024-02-11 18:32:34 +0300

    over view

---
+++ QuickSortAlgorithm/src/QuickSort.java
@@ -0,0 +1,11 @@
+public class QuickSort {
+
+    /*This method accepts a one dimensional array and sorts the data in ascending order*/
+
+    public int[] quickSort(int[] array)
+    {
+
+
+        return null;
+    }
+}
