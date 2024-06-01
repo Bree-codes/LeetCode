c2c4d5ae910d3a6a5a2605021f2b0ceadaecea8f
Bree-codes <mwongera.mukami22@students.dkut.ac.ke>
2024-06-01 20:39:51 +0300

    designing the binary search algo

--- insertionSort/src/Main.java
+++ insertionSort/src/Main.java
@@ -9,14 +9,25 @@ public class Main {
         * ----> First, loop to find the position to insert the number.
         * ----> technique => binary search the position to insert the number.
         * */
-        int len = arr.length, start = 0, mid;
+        int len = arr.length, start = 0, mid, insertionIndex;
 
         while(true){
             mid = start + len;
 
-            if(arr[mid] < num){
-
+            if((arr[mid] < num && arr[mid+1] > num) || arr[mid] == num){
+                insertionIndex = mid+1;
+                break;
             }
+
+            if((arr[mid] < num && !(arr[mid+1] > num))){
+                start = mid+1;
+            }
+
+            if(((arr[mid] > num) && arr[mid+1] > num) || arr[mid] == num){
+                insertionIndex = mid+1;
+                break;
+            }
+
         }
 
         return null;
