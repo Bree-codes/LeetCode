3f77c210f237af0dfc7769908d3f410b62519678
Bree-codes <mwongera.mukami22@students.dkut.ac.ke>
2024-06-02 18:04:46 +0300

    start locates the positon to insert the number.===> perfectos

--- insertionSort/src/Main.java
+++ insertionSort/src/Main.java
@@ -1,20 +1,21 @@
 public class Main {
     public static void main(String[] args) {
 
-        int[] arr = {23,24,26,27,28,29,30,36,42,48,52,60,66};
+        int[] arr = {23,26,27,28,29,30,31,37,48,50};
 
-        insertionSort(arr, 29);
+        insertionSort(arr, 390);
     }
 
     public static int[] insertionSort(int[] arr, int num) {
-
         /*
         * ----> First, loop to find the position to insert the number.
         * ----> technique => binary search the position to insert the number.
         * */
-        int len = arr.length, start = 0, mid = 0, insertionIndex;
+        int len = arr.length-1, start = 0, mid = 0, insertionIndex;
 
-        while(start < len) {
+        System.out.println("here we are");
+
+        while(start <= len) {
             mid = (start + len)/2;
 
             if(num == arr[mid]){
