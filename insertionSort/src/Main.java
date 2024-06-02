d0ab363d43ccdfab5a4c682ea5802f855109fb39
Bree-codes <mwongera.mukami22@students.dkut.ac.ke>
2024-06-02 17:51:39 +0300

    re designed the binary search to search the position for insertion..

--- insertionSort/src/Main.java
+++ insertionSort/src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
     public static void main(String[] args) {
 
+        int[] arr = {23,24,26,27,28,29,30,36,42,48,52,60,66};
+
+        insertionSort(arr, 29);
     }
 
     public static int[] insertionSort(int[] arr, int num) {
@@ -9,7 +12,22 @@ public class Main {
         * ----> First, loop to find the position to insert the number.
         * ----> technique => binary search the position to insert the number.
         * */
-        int len = arr.length, start = 0, mid, insertionIndex;
+        int len = arr.length, start = 0, mid = 0, insertionIndex;
+
+        while(start < len) {
+            mid = (start + len)/2;
+
+            if(num == arr[mid]){
+                System.out.println("mid = " + mid);
+                break;
+            } else if(num > arr[mid]){
+                start = mid + 1;
+            }else if (num < arr[mid]){
+                len = mid -1;
+            }
+        }
+
+        System.out.println("\nmid = "+mid+"\nstart = "+start+"\nlen = "+len);
 
         return null;
     }
