0e0d5517b918d9961463681da4d697cdf1aafcae
Bree-codes <mwongera.mukami22@students.dkut.ac.ke>
2024-06-03 05:38:07 +0300

    position location and insertion done, on to testing.

--- insertionSort/src/Main.java
+++ insertionSort/src/Main.java
@@ -1,9 +1,11 @@
 public class Main {
     public static void main(String[] args) {
 
-        int[] arr = {23,26,27,28,29,30,31,37,48,50};
+        int[] arr = {23,26,27,28,29,29,29,30,31,37,48,50,0};
 
-        insertionSort(arr, 390);
+        for(int i : insertionSort(arr, 29)){
+            System.out.print(i+"\t");
+        }
     }
 
     public static int[] insertionSort(int[] arr, int num) {
@@ -11,13 +13,14 @@ public class Main {
         * ----> First, loop to find the position to insert the number.
         * ----> technique => binary search the position to insert the number.
         * */
-        int len = arr.length-1, start = 0, mid = 0, insertionIndex;
+        int len = arr.length-2, start = 0, mid = 0, insertionIndex;
 
+        //start gives us the position where to insert the number.
+
+        /* ++>  Time Complexity ==>O(log(n))<==*/
         while(start <= len) {
             mid = (start + len)/2;
-
             if(num == arr[mid]){
-                System.out.println("mid = " + mid);
                 break;
             } else if(num > arr[mid]){
                 start = mid + 1;
@@ -26,6 +29,19 @@ public class Main {
             }
         }
 
-        return null;
+        /*if(arr[mid] == num)
+            start = mid;
+*/
+        System.out.println("start: " + start);
+        System.out.println("mid: " + mid);
+
+        //loop to create an empty slot for insertion of the number.
+        for(int i = arr.length-1; i >= start; i--){
+            arr[i] = arr[i-1];
+        }
+
+        arr[start] = num;
+
+        return arr;
     }
 }
