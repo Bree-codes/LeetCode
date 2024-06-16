283401f545951d7751b7ff8af56dab091c92ba1d
Bree-codes <mwongera.mukami22@students.dkut.ac.ke>
2024-06-16 17:00:34 +0300

    Finding largest and smallest number in an array

--- HighestNumberArray/src/Main.java
+++ HighestNumberArray/src/Main.java
@@ -3,7 +3,8 @@ public class Main {
 
         Solution solution = new Solution();
         solution.findLargest();
+        solution.findsmallest();
 
-        System.out.println("The largest number in the array is: "+ solution.largest);
+        System.out.println("The largest number in the array is: "+ solution.largest + " " + "\nThe smallest number in the array is: "+solution.smallest);
     }
 }
