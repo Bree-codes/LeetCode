2eef4839dc9e9fef9584e72855b3990d13e2c2d4
Bree-codes <mwongera.mukami22@students.dkut.ac.ke>
2024-06-17 14:48:48 +0300

    Working on reverse string

--- ReverseString/src/Main.java
+++ ReverseString/src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
 
+        Solution solution = new Solution();
+        solution.reverseString();
+        System.out.println("The reverse of " + solution.word + " is: " + solution.reversedString);
     }
 }
--- ReverseString/src/Solution.java
+++ ReverseString/src/Solution.java
@@ -1,2 +1,14 @@
 public class Solution {
+
+    String word = "Hello People!";
+
+    //create a new instance of string builder to store the reversed string
+    StringBuilder reversedString = new StringBuilder();
+
+    //method to reverse string
+    public void reverseString(){
+    for(int i = word.length()-1 ; i>=0 ; i--) {
+        reversedString.append(word.charAt(i));
+      }
+    }
 }
