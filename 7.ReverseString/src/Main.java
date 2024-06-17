ca6bdc52f9e31e320e9f27b01c42c9f1f262edda
Bree-codes <mwongera.mukami22@students.dkut.ac.ke>
2024-06-17 15:40:22 +0300

    Algo for reversing a 32-bit Integer

--- 7.ReverseString/src/Main.java
+++ 7.ReverseString/src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args){
 
+        Solution solution = new Solution();
+        int ans = solution.reverse(153423646);
+
+        System.out.println("The reverse is: " + ans);
     }
 }
