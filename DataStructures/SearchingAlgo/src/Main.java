6d76d77c013046e6499e897d9ca949fbd0c8008a
Bree-codes <mwongera.mukami22@students.dkut.ac.ke>
2024-06-24 18:36:10 +0300

    Arrays linearsearch algo

--- DataStructures/SearchingAlgo/src/Main.java
+++ DataStructures/SearchingAlgo/src/Main.java
@@ -3,5 +3,10 @@ public class Main {
 
         int[] arr = {2,5,8,10,11,12,14};
         int num = 10;
+
+        LinearSearch linearSearch = new LinearSearch();
+        int n = linearSearch.solution(arr,num);
+
+        System.out.println("The index is: " +n);
     }
 }
